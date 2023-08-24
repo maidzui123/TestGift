a9052e1f8426cc487d05917f92194501243e840e
maidzui123 <90997428+maidzui123@users.noreply.github.com>
2023-08-25 02:26:14 +0700

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -1,10 +1,11 @@
 import Toasty from "./toast.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 const signInEmail = document.getElementById("signin-email");
 const signInPass = document.getElementById("signin-password");
 const signInBtn = document.getElementById("signin-btn");
 const toastHTMLElement = document.getElementById("toast");
 const toastContent = document.getElementById("toast-content");
-
+const auth = getAuth();
 signInBtn.addEventListener("click", (e) => {
     e.preventDefault();
     var emailValueSI = signInEmail.value;
@@ -21,12 +22,23 @@ signInBtn.addEventListener("click", (e) => {
         signInToast.showAlert("Email or Password isn't correct :(", "red");
     }
     else {
+       signInWithEmailAndPassword(auth, emailValueSI, passwordValueSI)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
         async function signIn() {
-            signInToast.showAlert("Login Successful!", "green")
-            localStorage.setItem("checkPage", 1) 
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            window.location.href = 'index.html'
-          }
-          signIn();
+          signInToast.showAlert("Login Successful!", "green")
+          await new Promise(resolve => setTimeout(resolve, 1000)); 
+          window.location.href = 'index.html'
+
+        }
+        signIn();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        signInToast.showAlert("Email/Password is incorrect :(", "red")
+      });
     }
 });
